c88fd3d833330050a9373641fad67fdbbe345aea
Ameed Sayeh <ameed.say3h@gmail.com>
2020-06-15 13:50:38 +0300

    Initial Commit

---
+++ Animation Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation Test
+//
+//  Created by Ameed Sayeh on 6/15/20.
+//  Copyright © 2020 Ameed Sayeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
